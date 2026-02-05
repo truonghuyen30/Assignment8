1e674fd2af5711dccfdaddb70ec85d7645b69cb1
truonghuyen30 <baohuyen3009@gmail.com>
2026-02-05 07:24:28 +0700

    Update AccessModifier2.java

--- Lec11_Encapsulation/AccessModifier/DifferentPackageSubClass/Text2/AccessModifier2.java
+++ Lec11_Encapsulation/AccessModifier/DifferentPackageSubClass/Text2/AccessModifier2.java
@@ -8,10 +8,11 @@ public class AccessModifier2 extends AccessModifier1 {
     public static void main(String[] args) {
         AccessModifier2 obj = new AccessModifier2();
 
-        // System.out.println(obj.pvt); // ❌ private
-        // System.out.println(obj.df);  // ❌ default (khác package)
+        // System.out.println(obj.pvt); //  private
+        // System.out.println(obj.df);  //  default (khác package)
 
         System.out.println(obj.pro); 
         System.out.println(obj.pbl); 
     }
 }
+
